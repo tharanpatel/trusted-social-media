ef9b2210057b8989624c7eeed99f7f28ff38704a
tharanpatel <tharan451@gmail.com>
2024-12-10 17:04:51 +0000

    add gitignore and update connect.js

--- api/connect.js
+++ api/connect.js
@@ -4,6 +4,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root", // what the database username is
-    password: "Mysql123!", // what the database password is
+    password: "password", // what the database password is
     database: "tharansocial" //what the database name is on mysql
 })
